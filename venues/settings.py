fd43476b677665fcd2f8e222aacf3f3490e94a17
ANIKET SINGH <bruces1998@gmail.com>
2019-03-28 23:39:16 +0530

    Update settings.py

--- venues/settings.py
+++ venues/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'y2(a3^oti0iber0dv2-cb*&l)%cn3d)lgp=eg%u%h2dcmmhx=l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythonanywhere.com']
 
 
 # Application definition
